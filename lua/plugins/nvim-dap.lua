c9228a82c734b8375d6c197422535930c0af6c19
Javier Lucena Castro <javluccas@gmail.com>
2024-06-16 13:45:55 +0200

    Añadido ldap

--- lua/plugins/nvim-dap.lua
+++ lua/plugins/nvim-dap.lua
@@ -16,5 +16,33 @@ return {
         launch_scene = true,
       }
     }
+    local dap = require('dap')
+    
+    vim.keymap.set("n", "<F5>", dap.continue, { desc = 'Debug: Start/Continue'})
+    vim.keymap.set("n", "<F6>", function()
+        dap.terminate()
+    end, { desc = 'Debug: Stop'})
+    vim.keymap.set("n", "<F8>", dap.step_into, { desc = 'Debug: Step Into'})
+    vim.keymap.set("n", "<F7>", dap.step_over, { desc = 'Debug: Step Over'})
+    vim.keymap.set("n", "<F9>", dap.step_out, { desc = 'Debug: Step Out'})
+    vim.keymap.set("n", "<leader>b", dap.toggle_breakpoint, { desc = 'Debug: Toggle Breakpoint'})
+    vim.keymap.set("n", "<leader>B", function()
+    dap.set_breakpoint(vin.fn.input 'Breakpoint condition: ')
+    end, { desc = 'Debug: Toggle Breakpoint with condition'})
+
+    vim.keymap.set({'n', 'v'}, '<Leader>dh', function()
+      require('dap.ui.widgets').hover()
+    end)
+    vim.keymap.set({'n', 'v'}, '<Leader>dp', function()
+      require('dap.ui.widgets').preview()
+    end)
+    vim.keymap.set('n', '<Leader>df', function()
+      local widgets = require('dap.ui.widgets')
+      widgets.centered_float(widgets.frames)
+    end)
+    vim.keymap.set('n', '<Leader>ds', function()
+      local widgets = require('dap.ui.widgets')
+      widgets.centered_float(widgets.scopes)
+    end)
   end
 }
