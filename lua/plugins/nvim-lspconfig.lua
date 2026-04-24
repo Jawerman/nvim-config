56810cf765938b1fb17927188bd15348e3e752e4
Javier Lucena Castro <javluccas@gmail.com>
2026-04-24 07:41:41 +0200

    added blink.lib dependency

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -5,7 +5,13 @@ return {
     { "mason-org/mason-lspconfig.nvim" },
     "WhoIsSethDaniel/mason-tool-installer.nvim",
     { "j-hui/fidget.nvim", opts = {} },
-    "saghen/blink.cmp",
+    {
+      "saghen/blink.cmp",
+      dependencies = {
+        "saghen/blink.lib",
+        "rafamadriz/friendly-snippets",
+      },
+    },
   },
 
   config = function()
